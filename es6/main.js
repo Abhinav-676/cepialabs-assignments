e223ee0f14bfe165d54982d777ecb1582d7bf67a
Abhinav-676 <harshikeshabhinav670@gmail.com>
2025-11-05 16:15:50 +0000

    updated comment

--- es6/main.js
+++ es6/main.js
@@ -3,7 +3,7 @@ import { Expression } from "./Expression.js";
 
 const e = "30 - 15 / 3 * 4 / 2 + 5 - 10 * 80 + 55"
 
-// Follows BODMAS rule for expression evaluation
+// Follows BODMAS rule for expression evaluation (except for brackets and oders/exponents)
 const exp = new Expression(e);
 
 // Measure evaluate time
