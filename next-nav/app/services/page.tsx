45af159dc9f674a23275cbac8d7e17d208d7d893
Abhinav-676 <harshikeshabhinav670@gmail.com>
2025-11-26 13:08:30 +0000

    next-nav

---
+++ next-nav/app/services/page.tsx
@@ -0,0 +1,8 @@
+export default function Services() {
+  return (
+    <div className="text-center mt-10">
+      <h1 className="text-4xl font-bold mb-4">Our Services</h1>
+      <p className="text-gray-600">Check out the amazing services we offer.</p>
+    </div>
+  );
+}
